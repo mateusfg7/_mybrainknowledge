dd8776882048b5248d51439e42585ee4afae6548
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-09-13 18:04:29 -0300

    Import Posts

---
+++ new.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_date=$(date '+%Y-%m-%d')
+file="$1.md"
+
+echo "---" > $file
+echo "title: 'Draft Post'" >> $file
+echo "date: '$current_date'" >> $file
+echo "description: 'Post made for test markdown render'" >> $file
+echo "category: 'Draft'" >> $file
+echo "tags: 'title4test, content4test, code4test'" >> $file
+echo "---" >> $file
+
+echo "POST PATH: $file"
+
